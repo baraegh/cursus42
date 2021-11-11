7903bae4fbd78ef758e62960e86fee2a2691b150
Elbarae El-ghannouchi <eel-ghan@e2r2p2.1337.ma>
2021-11-11 18:41:37 +0100

    ft_memmove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:29:50 by eel-ghan          #+#    #+#             */
-/*   Updated: 2021/11/11 16:26:57 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2021/11/11 18:31:15 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,25 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char		*d;
 	const char	*s;
+	size_t		i;
 
 	d = dst;
 	s = src;
-	if (!d && !s)
+	if (!dst && !src)
 		return (0);
-	while (len -- > 0)
-		*d++ = *s++;
+	if (d > s)
+	{
+		while (len -- > 0)
+			d[len] = s[len];
+	}
+	else
+	{
+		i = 0;
+		while (i < len)
+		{
+			*(d + i) = *(s + i);
+			i++;
+		}
+	}
 	return (d);
 }
-
-// int main()
-// {
-// 	char s[] = {65, 66, 67, 68, 69, 0, 45};
-// 	//char s0[] = { 0,  0,  0,  0,  0,  0, 0};
-
-// 	printf("%s\n%s", (char*) memmove(s, s + 2, 0), (char*) ft_memmove(s, s + 2, 0));
-// }

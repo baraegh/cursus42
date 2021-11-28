edda534acd27320f231c0f2eba6eb5b9e61818c3
Elbarae El-ghannouchi <eel-ghan@e1r9p2.1337.ma>
2021-11-28 16:56:56 +0100

    ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 21:10:29 by eel-ghan          #+#    #+#             */
-/*   Updated: 2021/11/27 20:49:15 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2021/11/28 16:56:10 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,29 @@ void	ft_putnbr(int nb)
 	if (n < 0)
 	{
 		n *= -1;
-		write(1, "-", 1);
+		ft_putchar('-');
 	}
 	if (n >= 10)
-	{
 		ft_putnbr(n / 10);
-	}
 	number = 48 + (n % 10);
 	ft_putchar(number);
-	// write(1, &number, 1);
+}
+
+void ft_put_uint(unsigned int n)
+{
+	if (n < 0)
+		n = (UINT_MAX + 1) + n;
+	if (n >= 10)
+		ft_put_uint(n / 10);
+	ft_putchar(48 + (n % 10));
 }
 
 int ft_check(char c)
 {
-	char	s[] = "cspdiuxX%";
+	char	*s;
 	int		i;
-	// s = "cspdiuxX%";
+	
+	s = "cspdiuxX%";
 	i = 0;
 	while (*(s + i))
 	{
@@ -67,18 +74,52 @@ void	ft_putstr(char *str)
 {
 	while (*str != '\0')
 	{
-		write(1, str, 1);
+		ft_putchar(*str);
 		str++;
 	}
 }
 
+void ft_puthexa(int n, char *s)
+{
+	unsigned int nb;
+
+	nb = n;
+	if (nb < 0)
+		nb = (UINT_MAX + 1) + nb;
+	if (nb > 15)
+		ft_puthexa(nb / 16, s);
+	ft_putchar(s[nb % 16]);
+}
+
+void	ft_putptr(void *ptr)
+{
+	ft_putstr("0x7ffe");
+	ft_puthexa((unsigned)&(*ptr), "0123456789abcdef");
+}
+
 int ft_set_print(char c, va_list list)
 {
+	char	*x;
+	char	*X;
+
+	x = "0123456789abcdef";
+	X = "0123456789ABCDEF";
 	if (c == 'c')
 		ft_putchar(va_arg(list, int));
 	else if (c == 's')
 		ft_putstr(va_arg(list, char *));
-
+	else if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(list, int));
+	else if (c == 'u')
+		ft_put_uint(va_arg(list, unsigned int));
+	else if (c == '%')
+		ft_putchar('%');
+	else if (c == 'x')
+		ft_puthexa(va_arg(list, int), x);
+	else if (c == 'X')
+		ft_puthexa(va_arg(list, int), X);
+	else if (c == 'p')
+		ft_putptr(va_arg(list, void *));
 	return (0);
 }
 
@@ -86,10 +127,8 @@ int	ft_printf(const char *format, ...)
 {
 	va_list		list;
 	int			i;
-	int			str_len;
 	int			count;
-	
-	// str_len = ft_strlen(format);
+
 	va_start(list, format);
 	i = 0;
 	count = 0;
@@ -111,6 +150,10 @@ int	ft_printf(const char *format, ...)
 
 int main()
 {
-	ft_printf("%%");
-	// printf("\n%s", "abc");
+	int n = 2;
+	int *p = &n;
+
+	ft_printf("test : %p", p);
+	printf("\ntest : %p", p);
+	// printf("\ntest : 0x%u", (unsigned)&(*p));
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 21:16:00 by eel-ghan          #+#    #+#             */
-/*   Updated: 2021/11/27 19:56:55 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2021/11/28 15:37:11 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 /* TO BE DELETED */
 #include <stdio.h>
 /*################*/
